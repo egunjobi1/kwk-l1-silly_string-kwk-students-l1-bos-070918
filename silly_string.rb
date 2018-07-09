3fbcf9e09a19ab2c73ce0f748cb678249f562fa1
Oluwapelumi Egunjobi <pelumitegunjobi@gmail.com>
2018-07-09 20:13:13 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -25,21 +25,21 @@ big_word = "supercalifragilisticexpialidocious"
 
 letter_count = big_word # Put a . after big_word and use a String method.
 
-puts "There are #{letter_count} in #{big_word}"
+puts "There are #{big_word.length} in #{big_word}"
 
 # Challenge 5: Add "wow" and "mom" to big_word
 
-bigger_word = big_word # Use String concatenation to add "wow" and "mom" to big_word
+bigger_word = big_word + "wow" + "mom"
 
 new_letter_count = bigger_word.size
 
-puts "There are now #{new_letter_count} in #{bigger_word}"
+puts "There are now #{bigger_word.length} in #{bigger_word}"
 
 # Challenge 6: Capitalize this sentence properly
 
 proper_sentence = "i really like programming." # Put a . after the string to capitalize it properly.
 
-puts proper_sentence
+puts proper_sentence.capitalize
 
 # Challenge 7: String Switcheroo
 sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
